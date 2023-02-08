0d7730f93d63731c2d0562aa2d7ec16d8e7dcc2b
Tim Fuchs <tim@shakepay.co>
2023-02-07 20:45:15 -0500

    bootstrap editor

--- libs/editor/src/pages/index.tsx
+++ libs/editor/src/pages/index.tsx
@@ -1,9 +1,33 @@
 import React from 'react';
 
+import { Canvas } from 'reaflow';
+
 export default function Index() {
+  const nodes = [
+    {
+      id: '1',
+      text: '1',
+    },
+    {
+      id: '2',
+      text: '2',
+    },
+  ];
+
+  const edges = [
+    {
+      id: '1-2',
+      from: '1',
+      to: '2',
+    },
+  ];
+
   return (
     <div>
-      <h1>Hello world</h1>
+      <Canvas
+        nodes={nodes}
+        edges={edges}
+      />
     </div>
   );
 }
