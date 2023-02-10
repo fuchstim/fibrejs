d56747716e206baf7a4f6b33881c76815aabdc9c
Tim Fuchs <tim@shakepay.co>
2023-02-10 17:22:12 -0500

    full implementation

--- libs/engine/src/common/event-emitter.ts
+++ libs/engine/src/common/event-emitter.ts
@@ -16,4 +16,44 @@ export default class EventEmitter<T extends TEventTypeMap> extends BaseEventEmit
   emit<N extends TEventName<T>>(eventName: N, payload: T[N]) {
     return super.emit(eventName, payload);
   }
+
+  addListener<N extends TEventName<T>>(eventName: N, listener: TEventListener<T[N]>) {
+    return super.addListener(eventName, listener);
+  }
+
+  once<N extends TEventName<T>>(eventName: N, listener: TEventListener<T[N]>) {
+    return super.once(eventName, listener);
+  }
+
+  removeListener<N extends TEventName<T>>(eventName: N, listener: TEventListener<T[N]>) {
+    return super.removeListener(eventName, listener);
+  }
+
+  removeAllListeners<N extends TEventName<T>>(eventName: N) {
+    return super.removeAllListeners(eventName);
+  }
+
+  listeners<N extends TEventName<T>>(eventName: N): TEventListener<T[N]>[] {
+    return super.listeners(eventName) as TEventListener<T[N]>[];
+  }
+
+  rawListeners<N extends TEventName<T>>(eventName: N): TEventListener<T[N]>[] {
+    return super.rawListeners(eventName) as TEventListener<T[N]>[];
+  }
+
+  listenerCount<N extends TEventName<T>>(eventName: N): number {
+    return super.listenerCount(eventName);
+  }
+
+  prependListener<N extends TEventName<T>>(eventName: N, listener: TEventListener<T[N]>) {
+    return super.prependListener(eventName, listener);
+  }
+
+  prependOnceListener<N extends TEventName<T>>(eventName: N, listener: TEventListener<T[N]>) {
+    return super.prependOnceListener(eventName, listener);
+  }
+
+  eventNames(): (string | symbol)[] {
+    return super.eventNames();
+  }
 }
