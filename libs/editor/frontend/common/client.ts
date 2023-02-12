367f490970ee80fc4a47372d20e16343bc0ee48c
Tim Fuchs <tim@shakepay.co>
2023-02-12 00:31:58 -0500

    delete

--- libs/editor/frontend/common/client.ts
+++ libs/editor/frontend/common/client.ts
@@ -27,16 +27,23 @@ const wrappedGet = async <TResult>(url: string, data: Record<string, unknown> = 
 };
 
 // eslint-disable-next-line max-len
-const wrappedPost = async <TPayload extends Record<string, unknown>, TResult>(url: string, data: TPayload, config?: AxiosRequestConfig) => {
+const wrappedPost = async <TPayload>(url: string, data: TPayload, config?: AxiosRequestConfig) => {
   const result = await client.post(url, data, config)
     .catch(handleError);
 
-  return result.data as TResult;
+  return result.data as TPayload;
 };
 
 // eslint-disable-next-line max-len
-const wrappedPatch = async <TPayload extends Record<string, unknown>, TResult>(url: string, data: TPayload, config?: AxiosRequestConfig) => {
+const wrappedPatch = async <TPayload>(url: string, data: TPayload, config?: AxiosRequestConfig) => {
   const result = await client.patch(url, data, config)
+    .catch(handleError);
+
+  return result.data as TPayload;
+};
+
+const wrappedDelete = async <TResult>(url: string, config?: AxiosRequestConfig) => {
+  const result = await client.delete(url, config)
     .catch(handleError);
 
   return result.data as TResult;
@@ -54,11 +61,13 @@ export default {
 
   getRule: (ruleId: string) => wrappedGet<Types.Config.TRuleConfig>(`rules/${ruleId}`),
   findRules: () => wrappedGet<Types.Config.TRuleConfig[]>('rules'),
-  createRule: (rule: Types.Config.TRuleConfig) => wrappedPost('rules', rule),
-  updateRule: (rule: Types.Config.TRuleConfig) => wrappedPatch(`rules/${rule.id}`, rule),
+  createRule: (rule: Types.Config.TRuleConfig) => wrappedPost<Types.Config.TRuleConfig>('rules', rule),
+  updateRule: (rule: Types.Config.TRuleConfig) => wrappedPatch<Types.Config.TRuleConfig>(`rules/${rule.id}`, rule),
+  deleteRule: (ruleId: string) => wrappedDelete<Types.Config.TRuleConfig>(`rules/${ruleId}`),
 
   getRuleSet: (ruleId: string) => wrappedGet<Types.Config.TRuleSetConfig>(`rule-sets/${ruleId}`),
   findRuleSets: () => wrappedGet<Types.Config.TRuleSetConfig[]>('rule-sets'),
-  createRuleSet: (ruleSet: Types.Config.TRuleSetConfig) => wrappedPost('rule-sets', ruleSet),
-  updateRuleSet: (ruleSet: Types.Config.TRuleSetConfig) => wrappedPatch(`rule-sets/${ruleSet.id}`, ruleSet),
+  createRuleSet: (ruleSet: Types.Config.TRuleSetConfig) => wrappedPost<Types.Config.TRuleSetConfig>('rule-sets', ruleSet),
+  updateRuleSet: (ruleSet: Types.Config.TRuleSetConfig) => wrappedPatch<Types.Config.TRuleSetConfig>(`rule-sets/${ruleSet.id}`, ruleSet),
+  deleteRuleSet: (ruleSetId: string) => wrappedDelete<Types.Config.TRuleSetConfig>(`rule-sets/${ruleSetId}`),
 };
